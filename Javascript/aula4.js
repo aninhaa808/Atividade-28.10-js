cfaac4d82c6c8874eef4c751fe205b469e373e38
aninhaa808 <acarolinadeoliveira945@gmail.com>
2024-10-28 15:18:33 -0300

    Add files via upload

---
+++ Javascript/aula4.js
@@ -0,0 +1,18 @@
+function imprimirOláMundo() {
+console.log("Olá Mundo")
+}
+imprimirOláMundo()
+
+function nome(nome,sobrenome){
+    console.log(`Olá ${nome,sobrenome}`)
+}
+    nome("Maria","Clara")
+    nome("Isadora","Godoi")
+    nome("Ana","Carolina")
+
+
+    function numeros(numero1,numero2){
+    const numerais =numero1 + numero2
+    return numerais
+    }
+    console.log(numero= numeros(6,7))
